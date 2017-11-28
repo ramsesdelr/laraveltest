43b9e5633e8722d07fa865886e038156ff678934
Ramsés Del Rosario <ramsesdelr@gmail.com>
2017-11-28 18:28:54 -0400

    added first commit, created items, vendors and types models (with migration and controllers)

---
+++ database/factories/VendorsFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Vendors::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2017_11_28_213246_create_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('users_id');
+            $table->string('name', 200);
+            $table->integer('vendors_id');
+            $table->integer('types_id');
+            $table->string('sku', 30);
+            $table->string('price', 10);
+            $table->string('weight', 10)->nullable();
+            $table->string('color', 10)->nullable();
+            $table->date('release_date');
+            $table->string('photo')->nullable();
+            $table->longtext('tags');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
